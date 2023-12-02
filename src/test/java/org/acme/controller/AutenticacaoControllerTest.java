8a2b693b9ede2b701f72a5e30d104e0f6104bc6e
Enzo Rodrigues <enzorodrigues272@icloud.com>
2023-12-02 19:43:40 -0300

    tentando pela milésima vez

--- src/test/java/org/acme/controller/AutenticacaoControllerTest.java
+++ src/test/java/org/acme/controller/AutenticacaoControllerTest.java
@@ -28,28 +28,6 @@ public class AutenticacaoControllerTest {
     @Mock
     GeraTokenJWT geraTokenJWT;
 
-<<<<<<< HEAD
-=======
-    @Test
-    public void testLogin() { //testando o login - rodar esse teste com o projeto up
-
-    CadastroPF cadastroPF = new CadastroPF("Leonardo", "enzo@teste", "enzogabrielrodriguesdasilva", "ruma uva",
-    Role.USUARIO, "12345678910");
-    usuarioRepository.persist(cadastroPF);
-    when(usuarioRepository.findByCpf(cadastroPF.getCpf())).thenReturn(cadastroPF);
-    JsonObject loginUsuario = Json.createObjectBuilder()
-    .add("documento", cadastroPF.getCpf())
-    .add("senha", cadastroPF.getSenha())
-    .build();
-    given()
-    .contentType(MediaType.APPLICATION_JSON)
-    .body(loginUsuario.toString())
-    .when().post("http://localhost:8080/auth/login")
-    .then()
-    .statusCode(Response.Status.OK.getStatusCode());
-    }
-
->>>>>>> parent of 4c97b31 (atualizando teste e nome do cypress)
     @Test
     public void testIsCPF() { //testando método de validação do cpf
         AutenticacaoController controller = new AutenticacaoController();
