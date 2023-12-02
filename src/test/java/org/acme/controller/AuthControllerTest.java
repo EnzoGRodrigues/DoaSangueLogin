2242508ec93871499a8ca1cd645e4909059e5402
Enzo Rodrigues <enzorodrigues272@icloud.com>
2023-12-02 20:08:19 -0300

    alterando teste de api

--- src/test/java/org/acme/controller/AuthControllerTest.java
+++ src/test/java/org/acme/controller/AuthControllerTest.java
@@ -2,7 +2,9 @@ package org.acme.controller;
 
 import static io.restassured.RestAssured.given;
 
-
+import org.acme.dto.LoginDTO;
+import org.hamcrest.Matchers;
+import static org.hamcrest.Matchers.notNullValue;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -11,22 +13,40 @@ import io.restassured.response.Response;
 
 
 @QuarkusTest
-
 public class AuthControllerTest {
 
-    @Test
-    void testandoAPILogin(){ //teste de api login - precisa estar com o banco mysql rodando
-//        LoginDTO loginDTO = new LoginDTO("12345678910", "enzogabrielrodriguesdasilva");
-        Response response = given()
-        .contentType(ContentType.JSON)
-        .body("{\"documento\":\"12345678910\",\"senha\":\"enzogabrielrodriguesdasilva\"}")
-        .when().post("/auth/login");
+        @Test
+        public void deveRetornarStatus200ComTokenValido() {
+            // Define os dados de login
+            LoginDTO loginDTO = new LoginDTO("12345678910", "enzogabrielrodriguesdasilva");
+    
+            // Realiza a chamada HTTP
+            Response response = given()
+                .contentType(ContentType.JSON)
+                .body(loginDTO)
+                .when()
+                .post("/auth/login");
+    
+            // Verifica o status da resposta
+            response.then().statusCode(200);
+    
+            // Verifica a presença do token na resposta
+            response.then().body("token", notNullValue());
+        }
 
-        response.then().statusCode(200);
-//        response.then().body("token", Matchers.notNullValue());
-        response.then()
-                .log()
-                .all();
-    }
+//     @Test
+//     void testandoAPILogin(){ //teste de api login - precisa estar com o banco mysql rodando
+//         LoginDTO loginDTO = new LoginDTO("12345678910", "enzogabrielrodriguesdasilva");
+//         Response response = given()
+//         .contentType(ContentType.JSON)
+//         .body(loginDTO)
+//         .when().post("/auth/login");
+
+//         response.then().statusCode(200);
+//         response.then().body("token", Matchers.notNullValue());
+//         response.then()
+//                 .log()
+//                 .all();
+//     }
 
 }
