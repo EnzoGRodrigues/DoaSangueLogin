15f2d622f40451ecf1f428f5b4edc0b716db188e
Enzo Rodrigues <enzorodrigues272@icloud.com>
2023-12-02 19:59:15 -0300

    comentando teste de api

--- src/test/java/org/acme/controller/AuthControllerTest.java
+++ src/test/java/org/acme/controller/AuthControllerTest.java
@@ -1,32 +1,32 @@
-package org.acme.controller;
+// package org.acme.controller;
 
-import static io.restassured.RestAssured.given;
-
-
-import org.junit.jupiter.api.Test;
-
-import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
+// import static io.restassured.RestAssured.given;
 
 
-@QuarkusTest
+// import org.junit.jupiter.api.Test;
 
-public class AuthControllerTest {
+// import io.quarkus.test.junit.QuarkusTest;
+// import io.restassured.http.ContentType;
+// import io.restassured.response.Response;
 
-    @Test
-    void testandoAPILogin(){ //teste de api login - precisa estar com o banco mysql rodando
-//        LoginDTO loginDTO = new LoginDTO("12345678910", "enzogabrielrodriguesdasilva");
-        Response response = given()
-        .contentType(ContentType.JSON)
-        .body("{\"documento\":\"12345678910\",\"senha\":\"enzogabrielrodriguesdasilva\"}")
-        .when().post("/auth/login");
 
-        response.then().statusCode(200);
-//        response.then().body("token", Matchers.notNullValue());
-        response.then()
-                .log()
-                .all();
-    }
+// @QuarkusTest
 
-}
+// public class AuthControllerTest {
+
+//     @Test
+//     void testandoAPILogin(){ //teste de api login - precisa estar com o banco mysql rodando
+// //        LoginDTO loginDTO = new LoginDTO("12345678910", "enzogabrielrodriguesdasilva");
+//         Response response = given()
+//         .contentType(ContentType.JSON)
+//         .body("{\"documento\":\"12345678910\",\"senha\":\"enzogabrielrodriguesdasilva\"}")
+//         .when().post("/auth/login");
+
+//         response.then().statusCode(200);
+// //        response.then().body("token", Matchers.notNullValue());
+//         response.then()
+//                 .log()
+//                 .all();
+//     }
+
+// }
